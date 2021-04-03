a3b9fa7a19fd83712c5ce5a30e2b9b15cbd59ce3
umar-salihu <81760938+umar-salihu@users.noreply.github.com>
2021-04-03 14:58:12 +0100

    Add files via upload

---
+++ Csc104assigment.py
@@ -0,0 +1,17 @@
+name ="umar"
+dept ="science education"
+print(name,"\n,dept")
+scare="boo!"
+print (scare)
+n1= 3
+n2 = 5
+n3 = n1 + n2
+print ("n1=",n1"\nn2=",n2,"\nn3=n1+n2","\nn3=",n1+n2,"\n")
+rem=8\0.5
+print(rem)
+total=4+5
+print(total)
+rem=8%3
+print(rem)
+x=(2*4)*(4+2)
+print(x)
